b3d79b7f8808b4eee04c825779eda4864751e493
07Shriya <116485119+07Shriya@users.noreply.github.com>
2023-01-01 11:26:01 +0530

    Create PRODUCT.cpp
    This C++ program will give you product of two numbers

---
+++ PRODUCT.cpp
@@ -0,0 +1,12 @@
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+  clrscr();
+  float a,b,p=0;
+  cout<<"Enter two numbers " ;
+  cin>>a>>b;  
+  product=a*b;
+  cout<<"Product = "<<p;
+  getch();
+}  
