bdd2d3639d5fc532fc6c7480efe5b34f484fb765
07Shriya <116485119+07Shriya@users.noreply.github.com>
2023-01-01 11:12:59 +0530

    Create SUM.cpp
    This C++ program will add two numbers

---
+++ SUM.cpp
@@ -0,0 +1,12 @@
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+  clrscr();
+  int a,b,sum=0;
+  cout<<"Enter two numbers ;
+  cin>>a>>b;  
+  sum=a+b;
+  cout<<"Sum = "<<sum;
+  getch();
+}  
