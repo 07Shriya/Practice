30b4e9e61ddba7c5954bbfa9b7fbdbedbe2ec890
07Shriya <116485119+07Shriya@users.noreply.github.com>
2023-01-01 11:16:54 +0530

    Create DIFF.cpp
    This C++ program will tell the Difference between two numbers

---
+++ DIFF.cpp
@@ -0,0 +1,12 @@
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+  clrscr();
+  int a,b,diff=0;
+  cout<<"Enter two numbers " ;
+  cin>>a>>b;  
+  diff=a-b;
+  cout<<"Sum = "<<sum;
+  getch();
+}  
