ab301a31c7d9a740a790cd9ffff4965b69db7f4c
07Shriya <116485119+07Shriya@users.noreply.github.com>
2023-01-25 20:54:11 +0530

    Create public.cpp

---
+++ public.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+class xyz
+{
+	public:
+		double radius;
+		double result()
+		{
+			return 3.14*radius*radius;
+		}		
+};
+int main()
+{
+	xyz obj1;
+	obj1.radius=7;
+	cout<<"The area of circle with radius 7 is "<<obj1.result();
+	return(0);
+}
